34db83795e504a2ef964765f6c107ae16024a9e0
Sapna kumari soni <sapnakumarisoni1205@gmail.com>
2025-07-09 13:57:02 +0530

    Trigger webhook with print logging

--- app.py
+++ app.py
@@ -4,9 +4,9 @@
 # import os
 
 # app = Flask(__name__)
-# # client = MongoClient("mongodb://localhost:27017")
-# # db = client["webhook_db"]
-# # collection = db["github_events"]
+# client = MongoClient("mongodb://localhost:27017")
+# db = client["webhook_db"]
+# collection = db["github_events"]
 
 # @app.route('/')
 # def home():
